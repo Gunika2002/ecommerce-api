3e3e4760f4a458808b8adcf21edd3cf5583bd1e8
Gunika2002 <90132300+Gunika2002@users.noreply.github.com>
2023-07-20 22:46:18 +0530

    Update app.js
    changed body parser and added json input type

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/ecommerceDB");
 
